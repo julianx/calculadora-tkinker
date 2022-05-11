303815c89d8e77378210130cc5150d58b6819395
julian <julianx@gmail.com>
2022-05-11 12:55:02 -0700

    Anadiendo funcion para imprimir appelido.

--- main.py
+++ main.py
@@ -9,10 +9,16 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def imprimir_apellido(apellido):
+    print(f'{apellido}')
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Claudia!')
 
     print_hi('Ferney!')
+
+    imprimir_apellido("Uran")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
